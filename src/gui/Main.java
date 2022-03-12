b44f17153c36638ff07792e97e543a12a88ed229
Trooooooonix <andi_h@gmx.net>
2022-03-12 15:15:32 +0100

    GUI

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -5,14 +5,34 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
-        //Tasty Testing
-        System.out.println("Hallo Matthias");
-        System.out.println("markus commited");
+        /*
+            creating the window for the GUI
+         */
+        JFrame window = new JFrame("GPS-Tracker GUI");
+        window.setSize(1920,1080);
+        /*
+            adding the menubar to the window
+         */
+        JMenuBar menuBar = new JMenuBar();
+        window.add(menuBar);
+        /*
+            adding items to the Menubar
+         */
+        JMenuItem item1 = new JMenuItem("Option");
+        menuBar.add(item1);
+        JMenuItem item2 = new JMenuItem("File");
+        menuBar.add(item2);
+        JMenuItem item3 = new JMenuItem("Navigate");
+        menuBar.add(item3);
+        JMenuItem item4 = new JMenuItem("Refactor");
+        menuBar.add(item4);
+        JMenuItem item5 = new JMenuItem("Update View");
+        menuBar.add(item5);
 
-        JFrame frame = new JFrame("GPS-Tracker GUI");
-        frame.setSize(1920,1080);
-        frame.setVisible(true);
+
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
 
     }
 }
