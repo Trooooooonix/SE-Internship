7625f1ec9840c0e2cba36f6fe5e69f16184ff0bc
Trooooooonix <andi_h@gmx.net>
2022-03-12 15:07:09 +0100

    GUI

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,12 +1,18 @@
 package gui;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test");
         //Tasty Testing
         System.out.println("Hallo Matthias");
-
         System.out.println("markus commited");
+
+        JFrame frame = new JFrame("GPS-Tracker GUI");
+        frame.setSize(1920,1080);
+        frame.setVisible(true);
+
     }
 }
