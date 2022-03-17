87e9626db95d40e31c13390353bf88bf86984018
Trooooooonix <andi_h@gmx.net>
2022-03-17 17:12:27 +0100

    GUI changes

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -46,27 +46,83 @@ public class Main {
         menuBar.add(navigationMenu);
         JMenuItem option = new JMenuItem("Option");
         navigationMenu.add(option);
+        JMenuItem refresh = new JMenuItem("Refresh");
+        navigationMenu.add(refresh);
 
         /*
-            adding ActionListeners to react on click
+            adding actionListeners to react on click
          */
+        refresh.addActionListener(a -> window.repaint());
+
         option.addActionListener(a -> {
             window.setVisible(false);
             optionWindow.setVisible(true);
         });
 
-        JMenu fileMenu = new JMenu("Edit");
-        menuBar.add(fileMenu);
-        JMenuItem refresh = new JMenuItem("Refresh");
-        fileMenu.add(refresh);
 
-        /*
-            adding ActionListeners to react on click
-         */
-        refresh.addActionListener(a -> window.repaint());
+
+
 
         JMenu viewMenu = new JMenu("View");
         menuBar.add(viewMenu);
+
+
+        JMenu groupMenu = new JMenu("Group by");
+        menuBar.add(groupMenu);
+        JMenuItem year = new JMenuItem("Year");
+        groupMenu.add(year);
+        JMenuItem month = new JMenuItem("Month");
+        groupMenu.add(month);
+        JMenuItem day = new JMenuItem("Day");
+        groupMenu.add(day);
+
+        /*
+            Default: checked
+            if checked column is shown
+         */
+
+        JMenu columnMenu = new JMenu("Columns");
+        menuBar.add(columnMenu);
+        JCheckBoxMenuItem name = new JCheckBoxMenuItem("Name");
+        name.setSelected(true);
+        columnMenu.add(name);
+        JCheckBoxMenuItem date = new JCheckBoxMenuItem("Date");
+        date.setSelected(true);
+        columnMenu.add(date);
+        JCheckBoxMenuItem startTime = new JCheckBoxMenuItem("Start");
+        startTime.setSelected(true);
+        columnMenu.add(startTime);
+        JCheckBoxMenuItem distance = new JCheckBoxMenuItem("Distance");
+        distance.setSelected(true);
+        columnMenu.add(distance);
+        JCheckBoxMenuItem pace = new JCheckBoxMenuItem("Pace");
+        pace.setSelected(true);
+        columnMenu.add(pace);
+        JCheckBoxMenuItem averageBpm = new JCheckBoxMenuItem("Average Bpm");
+        averageBpm.setSelected(true);
+        columnMenu.add(averageBpm);
+        JCheckBoxMenuItem maxBpm = new JCheckBoxMenuItem("max Bpm");
+        maxBpm.setSelected(true);
+        columnMenu.add(maxBpm);
+        JCheckBoxMenuItem heightlvl = new JCheckBoxMenuItem("height");
+        heightlvl.setSelected(true);
+        columnMenu.add(heightlvl);
+
+        JMenu track = new JMenu("Track");
+        menuBar.add(track);
+        JMenuItem cycling = new JMenuItem("Cycling");
+        track.add(cycling);
+        JMenuItem driving = new JMenuItem("Driving");
+        track.add(driving);
+        JMenuItem flying = new JMenuItem("Flying");
+        track.add(flying);
+        JMenuItem hiking = new JMenuItem("Hiking");
+        track.add(hiking);
+        JMenuItem running = new JMenuItem("Running");
+        track.add(running);
+        JMenuItem skiing = new JMenuItem("Skiing");
+        track.add(skiing);
+
 
         JMenu helpMenu = new JMenu("Help");
         menuBar.add(helpMenu);
@@ -74,13 +130,13 @@ public class Main {
         /*
             adding internal Frames to show specific Data
             Size is not working right now, should be in the right top of the window-frame and show the Overview
-         */
-        JInternalFrame internalFrame = new JInternalFrame("Übersicht", true, false, false, false);
-        internalFrame.setSize(200, 200);
-        internalFrame.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.BLACK));
-        internalFrame.setVisible(true);
-        window.add(internalFrame, BorderLayout.NORTH);
 
+            JInternalFrame internalFrame = new JInternalFrame("Übersicht", true, false, false, false);
+            internalFrame.setSize(200, 200);
+            internalFrame.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.BLACK));
+            internalFrame.setVisible(true);
+            window.add(internalFrame, BorderLayout.NORTH);
+        */
 
         window.setVisible(true);
     }
