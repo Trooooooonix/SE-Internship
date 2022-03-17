40a16b6aba4fc730a6ee677dcf27841eed1f04b8
Trooooooonix <andi_h@gmx.net>
2022-03-17 12:02:07 +0100

    GUI changes

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -13,18 +13,32 @@ public class Main {
             creating the window for the GUI
             setting a windowsize and a default operation to close upon clicking the "X" in the top-right
          */
-        JFrame window = new JFrame("GPS-Tracker GUI");
+        JFrame window = new JFrame("GPS-Tracker v420.69");
         window.setSize(1920,1080);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JFrame optionWindow = new JFrame("Options");
         optionWindow.setSize(1220,980);
         optionWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /*
+            adding a button to go back from the optin-window to the main-window
+         */
+        JButton doneButton = new JButton("Done");
+        doneButton.setFont(new Font("MV Boli", Font.BOLD, 20));
+        doneButton.setSize(200, 200);
+        optionWindow.add(doneButton, BorderLayout.SOUTH);
+        doneButton.addActionListener(a -> {
+            optionWindow.setVisible(false);
+            window.setVisible(true);
+        });
+
         /*
             adding the menubar to the main WINDOW (variableName = window)
          */
         JMenuBar menuBar = new JMenuBar();
         window.setJMenuBar(menuBar);
+
         /*
             adding Menus to the Menubar
          */
@@ -32,6 +46,7 @@ public class Main {
         menuBar.add(navigationMenu);
         JMenuItem option = new JMenuItem("Option");
         navigationMenu.add(option);
+
         /*
             adding ActionListeners to react on click
          */
@@ -44,6 +59,7 @@ public class Main {
         menuBar.add(fileMenu);
         JMenuItem refresh = new JMenuItem("Refresh");
         fileMenu.add(refresh);
+
         /*
             adding ActionListeners to react on click
          */
@@ -52,19 +68,19 @@ public class Main {
         JMenu viewMenu = new JMenu("View");
         menuBar.add(viewMenu);
 
-
         JMenu helpMenu = new JMenu("Help");
         menuBar.add(helpMenu);
 
+        /*
+            adding internal Frames to show specific Data
+            Size is not working right now, should be in the right top of the window-frame and show the Overview
+         */
+        JInternalFrame internalFrame = new JInternalFrame("Übersicht", true, false, false, false);
+        internalFrame.setSize(200, 200);
+        internalFrame.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.BLACK));
+        internalFrame.setVisible(true);
+        window.add(internalFrame, BorderLayout.NORTH);
 
-        JButton doneButton = new JButton("Done");
-        doneButton.setFont(new Font("MV Boli", Font.BOLD, 20));
-        doneButton.setSize(200, 200);
-        optionWindow.add(doneButton, BorderLayout.SOUTH);
-        doneButton.addActionListener(a -> {
-            optionWindow.setVisible(false);
-            window.setVisible(true);
-        });
 
 
 
