22caeb7733772912157cbc2ab09d73d5370ffb2e
Matthias <schwarz.matthias@drei.at>
2022-03-23 12:00:38 +0100

    GUI Design

--- src/gui/GpsTrackerGUI.java
+++ src/gui/GpsTrackerGUI.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class GpsTrackerGUI extends JFrame{
 
+    private JPanel panel1;
+
     public GpsTrackerGUI(String title){
         super(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
