96ca76528907e2ab8ba7ad95c256a7ade71b403e
Matthias <schwarz.matthias@drei.at>
2022-03-23 12:04:31 +0100

    GUI Design

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame window = new GpsTrackerGUI("GPS - Tracker v420.69");
+        window.setSize(500, 300);
         window.setVisible(true);
     }
 }
