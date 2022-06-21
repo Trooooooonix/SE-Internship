4369202a5273cd1de016922c741ff5a17c1fe501
Trooooooonix <andi_h@gmx.net>
2022-06-21 16:18:35 +0200

    Comparable + javaDoc + BugFix + added JUnitTest to classpath and set up class + init and tearDown method

---
+++ GPSTracker/src/test/TestGPSTracker.java
@@ -0,0 +1,28 @@
+package test;
+
+import app.GPSTracker;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestGPSTracker{
+
+    private GPSTracker gpsTracker;
+
+    @BeforeEach
+    public void init(){
+        this.gpsTracker = new GPSTracker();
+    }
+
+    @AfterAll
+    public void tearDown(){
+        this.gpsTracker = null;
+    }
+
+    //TODO: Test Methods and functions in Activity-class
+
+    @Test
+    public void testAnything(){
+
+    }
+}
